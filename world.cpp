7073613376a718397da53a1e269bfd4ad856bcb0
tinydumb <adustoevsky@gmail.com>
2022-03-03 21:05:59 +0300

    Update and render methods

--- world.cpp
+++ world.cpp
@@ -6,7 +6,7 @@ World::World(sf::Vector2u &wind_size) : m_window_size(wind_size){
     m_apple.setFillColor(sf::Color::Red);
     m_apple.setRadius(m_block_size/2);
 
-    for (int i = 0; i < 4; ++i){
+    for (int i = 0; i < 4; ++i) {
         m_bounds[i].setFillColor(sf::Color::Red);
         // !!!
         if(i%2!=0) {
@@ -22,6 +22,7 @@ World::World(sf::Vector2u &wind_size) : m_window_size(wind_size){
         }
     }
 }
+World::~World(){}
 void World::respawn_apple(){
     int max_x=(m_window_size.x / m_block_size)-2;
     int max_y=(m_window_size.y / m_block_size)-2;
@@ -29,4 +30,22 @@ void World::respawn_apple(){
     m_item = sf::Vector2i(rand() % max_x+1, rand() % max_y+1);
     m_apple.setPosition(m_item.x*m_block_size,m_item.y*m_block_size);
 }
-World::~World(){}
+void World::update(Snake &player){
+    if(player.get_position() == m_item) {
+        player.extend();
+        player.increase_score();
+        respawn_apple();
+    }
+    int grid_x_size=m_window_size.x/m_block_size;
+    int grid_y_size=m_window_size.y/m_block_size;
+    //TODO: make separate function for checking collisions with walls
+    if(player.get_position().x >= grid_x_size-1 || player.get_position().x <=0 || player.get_position().y<=0 || player.get_position().y>=grid_y_size-1) {
+        player.lose();
+    }
+}
+void World::render(sf::RenderWindow &window){
+    for (int i = 0; i < 4; ++i) {
+        window.draw(m_bounds[i]);
+    }
+    window.draw(m_apple);
+}
