e27dbbeda4f4419e2b853bed2fd260147935be79
tinydumb <adustoevsky@gmail.com>
2022-03-05 22:19:22 +0300

    'Binding' and 'EventDetails' structs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include "game.h"
 #include <iostream>
-int main()
-{
+
+int main(){
     Game game{};
 	while(!game.get_window()->is_done()){
         game.handle_input();
